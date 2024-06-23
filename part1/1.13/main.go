d14ad92d165c1b404d955421224d341643a1fbb1
David Andrea <davejandrea@gmail.com>
2024-06-23 17:25:42 -0300

    sync progress

---
+++ part1/1.13/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+
+	_, err := readTimestamp()
+	if err != nil {
+		writeTimestamp()
+		getImage()
+	}
+
+	go startTimestampWatcher()
+
+	r := gin.Default()
+	r.Static("/static", "./tmp/kube")
+	r.LoadHTMLGlob("templates/*")
+
+	// Full Disclosure: HTML mostly generated with ChatGPT and I inserted some templating stuff into it.
+	r.GET("/", func(c *gin.Context) {
+		todos := []string{"Buy milk", "Walk the dog", "Do laundry"}
+		c.HTML(http.StatusOK, "index.html", gin.H{"Todos": todos})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("*****************************")
+	fmt.Printf("Server started in port %s\n", port)
+	fmt.Println("*****************************\n")
+
+	r.Run()
+}
+
+func getImage() {
+	//for true {
+	resp, err := http.Get("https://picsum.photos/1200")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	filename := "tmp/kube/img.jpg"
+
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0644)
+
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	//file.Truncate(0)
+	//file.Seek(0, 0)
+
+	// Write the timestamp to the file
+	if _, err := io.Copy(file, resp.Body); err != nil {
+		fmt.Println("Error writing to file:", err)
+	}
+
+	fmt.Println("Image updated")
+	//time.Sleep(1 * time.Hour)
+	//}
+}
+
+func readTimestamp() (string, error) {
+	var filename = "tmp/kube/timestamp.txt"
+	var timestamp string
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+	}
+	defer file.Close()
+
+	// Create a new scanner to read the file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		timestamp = scanner.Text()
+	}
+
+	// Check for errors during scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	return timestamp, nil
+}
+
+func writeTimestamp() {
+	var filename = "tmp/kube/timestamp.txt"
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	timestamp := time.Now().Format(time.RFC3339)
+
+	file.Truncate(0)
+	file.Seek(0, 0)
+	// Write the timestamp to the file
+	if _, err := file.WriteString(timestamp + "\n"); err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+	fmt.Println("Timestamp written to file:", timestamp)
+}
+
+func checkTimestamp() bool {
+	//for true {
+	timestamp, err1 := readTimestamp()
+	if err1 != nil {
+		fmt.Println("error reading timestamp")
+	}
+	t, err2 := time.Parse(time.RFC3339, timestamp)
+	if err2 != nil {
+		fmt.Println("error parsing timestamp")
+	}
+	return time.Since(t) > time.Hour
+	//}
+}
+
+func startTimestampWatcher() {
+	fmt.Println("Starting timestamp observer")
+	for true {
+		if checkTimestamp() {
+			writeTimestamp()
+			getImage()
+		}
+		time.Sleep(1 * time.Hour)
+	}
+}
